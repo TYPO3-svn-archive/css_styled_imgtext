5d6ebe27047c8131d24a4743b733f37d94acdedc
a_schwarzkopf <a_schwarzkopf@735d13b6-9817-0410-8766-e36946ffe9aa>
2005-05-09 21:49:06 +0000

    syncronized version with the TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/css_styled_imgtext/trunk@325 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "css_styled_imgtext"
 # 
-# Auto generated 12-04-2005 23:26
+# Auto generated 09-05-2005 23:45
 # 
 # Manual updates:
 # Only the data in the array - anything else is removed by next write
@@ -21,22 +21,22 @@ $EM_CONF[$_EXTKEY] = Array (
 	'TYPO3_version' => '-',
 	'PHP_version' => '-',
 	'module' => '',
-	'state' => 'experimental',
+	'state' => 'beta',
 	'internal' => 0,
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'author' => 'Ingmar Schlecht for the Typo3 GUI Team',
-	'author_email' => 'ingmars@web.de',
+	'author' => 'Ingmar Schlecht, Andreas Schwarzkopf',
+	'author_email' => 'schwarzkopf.no.spam@artplan21.de',
 	'author_company' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
 	'private' => 0,
 	'download_password' => '',
-	'version' => '0.0.2',	// Don't modify this! Managed automatically during upload to repository.
-	'_md5_values_when_last_written' => 'a:12:{s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"62b0";s:14:"ext_tables.php";s:4:"1d0c";s:14:"ext_tables.sql";s:4:"b718";s:16:"localconf_db.php";s:4:"6fcb";s:16:"locallang_db.php";s:4:"3f30";s:19:"doc/wizard_form.dat";s:4:"10a1";s:20:"doc/wizard_form.html";s:4:"b0ff";s:37:"pi1/class.tx_cssstyledimgtext_pi1.php";s:4:"4730";s:38:"pi1/class.tx_cssstyledimgtext_pi1.php~";s:4:"f5f5";s:16:"static/setup.txt";s:4:"9690";s:17:"static/setup.txt~";s:4:"3239";}',
+	'version' => '0.1.0',	// Don't modify this! Managed automatically during upload to repository.
+	'_md5_values_when_last_written' => 'a:12:{s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"62b0";s:14:"ext_tables.php";s:4:"67f1";s:14:"ext_tables.sql";s:4:"b718";s:16:"localconf_db.php";s:4:"6fcb";s:16:"locallang_db.php";s:4:"3f30";s:19:"doc/wizard_form.dat";s:4:"10a1";s:20:"doc/wizard_form.html";s:4:"b0ff";s:37:"pi1/class.tx_cssstyledimgtext_pi1.php";s:4:"4730";s:38:"pi1/class.tx_cssstyledimgtext_pi1.php~";s:4:"f5f5";s:16:"static/setup.txt";s:4:"61d2";s:17:"static/setup.txt~";s:4:"3239";}',
 );
 
 ?>
